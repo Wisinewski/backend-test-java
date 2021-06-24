2e51bdcdd950a6c722c4b65457f664d72a4fdaa1
Wisinewski <g_gomees@hotmail.com>
2021-06-24 14:42:27 -0300

    feat: create AddEstablishment usecase

---
+++ src/main/java/com/wisinewski/backendtestjava/domain/usecases/AddEstablishment.java
@@ -0,0 +1,9 @@
+package com.wisinewski.backendtestjava.domain.usecases;
+
+import com.wisinewski.backendtestjava.domain.models.establishment.Establishment;
+
+public interface AddEstablishment {
+
+	void addEstablishment(Establishment establishment);
+	
+}
