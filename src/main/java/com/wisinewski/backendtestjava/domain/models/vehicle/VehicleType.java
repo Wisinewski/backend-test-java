506197681797ebc19f18cbc4faaa02c3a07b421f
Wisinewski <g_gomees@hotmail.com>
2021-06-24 14:40:20 -0300

    feat: create VehicleType entity

---
+++ src/main/java/com/wisinewski/backendtestjava/domain/models/vehicle/VehicleType.java
@@ -0,0 +1,60 @@
+package com.wisinewski.backendtestjava.domain.models.vehicle;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vehicleTypes")
+public class VehicleType implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String type;
+	
+	public VehicleType(Long id, String type) {
+		this.id = id;
+		this.type = type;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VehicleType other = (VehicleType) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
