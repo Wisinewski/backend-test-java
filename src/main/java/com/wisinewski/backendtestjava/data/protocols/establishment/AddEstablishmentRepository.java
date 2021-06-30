6553e9d4e152c5036c04c1b20368de9625c61dba
Wisinewski <g_gomees@hotmail.com>
2021-06-30 15:10:40 -0300

    feat: create AddEstablishmentRepository protocol

--- src/main/java/com/wisinewski/backendtestjava/data/protocols/establishment/AddEstablishmentRepository.java
+++ src/main/java/com/wisinewski/backendtestjava/data/protocols/establishment/AddEstablishmentRepository.java
@@ -1,4 +1,4 @@
-package com.wisinewski.backendtestjava.data.protocols;
+package com.wisinewski.backendtestjava.data.protocols.establishment;
 
 import com.wisinewski.backendtestjava.domain.models.establishment.Establishment;
 
