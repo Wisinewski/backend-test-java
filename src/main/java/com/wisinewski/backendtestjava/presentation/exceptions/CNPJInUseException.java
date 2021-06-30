1d69d785b731a304d9b469d9258da2bf8d9c6e8a
Wisinewski <g_gomees@hotmail.com>
2021-06-30 15:11:40 -0300

    feat: create CNPJInUse exception

--- src/main/java/com/wisinewski/backendtestjava/presentation/exceptions/CNPJInUseException.java
+++ src/main/java/com/wisinewski/backendtestjava/presentation/exceptions/CNPJInUseException.java
@@ -3,5 +3,9 @@ package com.wisinewski.backendtestjava.presentation.exceptions;
 public class CNPJInUseException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
+	
+	public CNPJInUseException() {
+		super("The received cnpj is already in use");
+	}
 
 }
