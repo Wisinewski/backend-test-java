a8f9dc48015c83f615b3a2c8c29b1c1d1aeca556
Wisinewski <g_gomees@hotmail.com>
2021-06-30 15:13:29 -0300

    feat: ensure EstablishmentMySQLRepository saves an establishment on success

---
+++ src/main/java/com/wisinewski/backendtestjava/infra/repository/db/mysql/EstablishmentMySQLRepository.java
@@ -0,0 +1,22 @@
+package com.wisinewski.backendtestjava.infra.repository.db.mysql;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.wisinewski.backendtestjava.data.protocols.establishment.AddEstablishmentRepository;
+import com.wisinewski.backendtestjava.data.protocols.establishment.LoadEstablishmentByCNPJRepository;
+import com.wisinewski.backendtestjava.domain.models.establishment.Establishment;
+
+public interface EstablishmentMySQLRepository extends JpaRepository<Establishment, Long>, AddEstablishmentRepository, LoadEstablishmentByCNPJRepository {
+
+	@Override
+	public default void add(Establishment establishment) {
+		save(establishment);
+	}
+	
+	@Override
+	@Query("SELECT e FROM Establishment e WHERE e.cnpj = :cnpj")
+	public Establishment loadByCNPJ(@Param(value = "cnpj") String cnpj);
+	
+}
