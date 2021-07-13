e44d12f097b905a657407794a3e8976cd52327bd
Wisinewski <g_gomees@hotmail.com>
2021-07-13 14:20:46 -0300

    feat: ensure EstablishmentMySQLRepository extends LoadEstablishmentByIdRepository

--- src/main/java/com/wisinewski/backendtestjava/infra/repository/db/mysql/EstablishmentMySQLRepository.java
+++ src/main/java/com/wisinewski/backendtestjava/infra/repository/db/mysql/EstablishmentMySQLRepository.java
@@ -1,14 +1,17 @@
 package com.wisinewski.backendtestjava.infra.repository.db.mysql;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.wisinewski.backendtestjava.data.protocols.LoadEstablishmentByCNPJRepository;
 import com.wisinewski.backendtestjava.data.protocols.establishment.AddEstablishmentRepository;
+import com.wisinewski.backendtestjava.data.protocols.establishment.LoadEstablishmentByIdRepository;
 import com.wisinewski.backendtestjava.domain.models.establishment.Establishment;
 
-public interface EstablishmentMySQLRepository extends JpaRepository<Establishment, Long>, AddEstablishmentRepository, LoadEstablishmentByCNPJRepository {
+public interface EstablishmentMySQLRepository extends JpaRepository<Establishment, Long>, AddEstablishmentRepository, LoadEstablishmentByCNPJRepository, LoadEstablishmentByIdRepository {
 
 	@Override
 	public default void add(Establishment establishment) {
@@ -18,5 +21,10 @@ public interface EstablishmentMySQLRepository extends JpaRepository<Establishmen
 	@Override
 	@Query("SELECT e FROM Establishment e WHERE e.cnpj = :cnpj")
 	public Establishment loadByCNPJ(@Param(value = "cnpj") String cnpj);
+	
+	@Override
+	default Optional<Establishment> loadById(Long id) {
+		return findById(id);
+	}
 	
 }
