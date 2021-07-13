2c337027324d924fb288c1a440acf92c748f7449
Wisinewski <g_gomees@hotmail.com>
2021-07-13 15:05:56 -0300

    feat: ensure EstablishmentMySQLRepository extends DeleteEstablishmentByIdRepository

--- src/main/java/com/wisinewski/backendtestjava/data/usecases/establishment/DbDeleteEstablishmentById.java
+++ src/main/java/com/wisinewski/backendtestjava/data/usecases/establishment/DbDeleteEstablishmentById.java
@@ -1,10 +1,12 @@
 package com.wisinewski.backendtestjava.data.usecases.establishment;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.wisinewski.backendtestjava.data.protocols.establishment.DeleteEstablishmentByIdRepository;
 import com.wisinewski.backendtestjava.domain.usecases.DeleteEstablishmentById;
 
+@Service
 public class DbDeleteEstablishmentById implements DeleteEstablishmentById {
 
 	private DeleteEstablishmentByIdRepository deleteEstablishmentByIdRepository;
