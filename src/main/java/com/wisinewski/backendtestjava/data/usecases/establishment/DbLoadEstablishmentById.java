12acb0cba09e034694eb8c49401f18c21575a9d5
Wisinewski <g_gomees@hotmail.com>
2021-07-13 14:22:15 -0300

    feat: create LoadEstablishmentById controller

--- src/main/java/com/wisinewski/backendtestjava/data/usecases/establishment/DbLoadEstablishmentById.java
+++ src/main/java/com/wisinewski/backendtestjava/data/usecases/establishment/DbLoadEstablishmentById.java
@@ -3,11 +3,13 @@ package com.wisinewski.backendtestjava.data.usecases.establishment;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.wisinewski.backendtestjava.data.protocols.establishment.LoadEstablishmentByIdRepository;
 import com.wisinewski.backendtestjava.domain.models.establishment.Establishment;
 import com.wisinewski.backendtestjava.domain.usecases.LoadEstablishmentById;
 
+@Service
 public class DbLoadEstablishmentById implements LoadEstablishmentById {
 
 	private LoadEstablishmentByIdRepository loadEstablishmentByIdRepository;
