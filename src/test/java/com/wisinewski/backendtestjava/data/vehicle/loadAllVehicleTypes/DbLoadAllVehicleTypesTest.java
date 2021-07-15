b638afcca31f0576967048f115953a21186ebe59
Wisinewski <g_gomees@hotmail.com>
2021-07-15 15:12:11 -0300

    test: ensure DbLoadAllVehicleTypes throws if LoadAllVehicleTypesRepository throws

--- src/test/java/com/wisinewski/backendtestjava/data/vehicle/loadAllVehicleTypes/DbLoadAllVehicleTypesTest.java
+++ src/test/java/com/wisinewski/backendtestjava/data/vehicle/loadAllVehicleTypes/DbLoadAllVehicleTypesTest.java
@@ -1,7 +1,10 @@
 package com.wisinewski.backendtestjava.data.vehicle.loadAllVehicleTypes;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.wisinewski.backendtestjava.data.usecases.vehicle.DbLoadAllVehicleTypes;
@@ -17,11 +20,24 @@ public class DbLoadAllVehicleTypesTest {
 		dbLoadAllVehicleTypes = new DbLoadAllVehicleTypes(loadAllVehicleTypesRepositorySpy);
 	}
 	
+	public void makeSut(LoadAllVehicleTypesRepositorySpy loadAllVehicleTypesRepositorySpy) {
+		this.loadAllVehicleTypesRepositorySpy = loadAllVehicleTypesRepositorySpy;
+		dbLoadAllVehicleTypes = new DbLoadAllVehicleTypes(this.loadAllVehicleTypesRepositorySpy);
+	}
+	
 	@Test
 	public void should_call_LoadAllVehicleTypesRepository() {
 		makeSut();
 		dbLoadAllVehicleTypes.loadAll();
 		Assert.assertTrue(loadAllVehicleTypesRepositorySpy.getWasCalled());
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void should_throw_if_LoadAllVehicleTypesRepository_throws() {
+		loadAllVehicleTypesRepositorySpy = Mockito.mock(LoadAllVehicleTypesRepositorySpy.class);
+		makeSut(loadAllVehicleTypesRepositorySpy);
+		when(loadAllVehicleTypesRepositorySpy.loadAll()).thenThrow(new RuntimeException());
+		dbLoadAllVehicleTypes.loadAll();
 	}
 	
 }
