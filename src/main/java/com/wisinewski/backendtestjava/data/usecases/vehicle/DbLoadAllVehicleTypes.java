1185b39a8df253d820878212c4730846bacb84ac
Wisinewski <g_gomees@hotmail.com>
2021-07-15 15:07:36 -0300

    feat: ensure DbLoadAllVehicleTypes calls LoadAllVehicleTypesRepository

---
+++ src/main/java/com/wisinewski/backendtestjava/data/usecases/vehicle/DbLoadAllVehicleTypes.java
@@ -0,0 +1,27 @@
+package com.wisinewski.backendtestjava.data.usecases.vehicle;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wisinewski.backendtestjava.data.protocols.vehicle.LoadAllVehicleTypesRepository;
+import com.wisinewski.backendtestjava.domain.models.vehicle.VehicleType;
+import com.wisinewski.backendtestjava.domain.usecases.LoadAllVehicleTypes;
+
+@Service
+public class DbLoadAllVehicleTypes implements LoadAllVehicleTypes {
+	
+	private LoadAllVehicleTypesRepository loadAllVehicleTypesRepository;
+	
+	@Autowired
+	public DbLoadAllVehicleTypes(LoadAllVehicleTypesRepository loadAllVehicleTypesRepository) {
+		this.loadAllVehicleTypesRepository = loadAllVehicleTypesRepository;
+	}
+
+	public List<VehicleType> loadAll() {
+		loadAllVehicleTypesRepository.loadAll();
+		return null;
+	}
+	
+}
