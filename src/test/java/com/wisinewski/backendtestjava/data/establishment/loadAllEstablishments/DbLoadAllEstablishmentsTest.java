db2cc3f7257ec9efa7e83c4810fbeafc3097a970
Wisinewski <g_gomees@hotmail.com>
2021-07-15 13:34:02 -0300

    test: ensure DbLoadAllEstablishments throws if LoadAllEstablishmentsRepository throws

--- src/test/java/com/wisinewski/backendtestjava/data/establishment/loadAllEstablishments/DbLoadAllEstablishmentsTest.java
+++ src/test/java/com/wisinewski/backendtestjava/data/establishment/loadAllEstablishments/DbLoadAllEstablishmentsTest.java
@@ -1,7 +1,10 @@
 package com.wisinewski.backendtestjava.data.establishment.loadAllEstablishments;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.wisinewski.backendtestjava.data.usecases.establishment.DbLoadAllEstablishments;
@@ -17,11 +20,24 @@ public class DbLoadAllEstablishmentsTest {
 		dbLoadAllEstablishments = new DbLoadAllEstablishments(loadAllEstablishmentsRepositorySpy);
 	}
 	
+	public void makeSut(LoadAllEstablishmentsRepositorySpy injectedLoadAllEstablishmentsRepositorySpy) {
+		loadAllEstablishmentsRepositorySpy = injectedLoadAllEstablishmentsRepositorySpy;
+		dbLoadAllEstablishments = new DbLoadAllEstablishments(loadAllEstablishmentsRepositorySpy);
+	}
+	
 	@Test
 	public void should_call_LoadAllEstablishmentsRepository() {
 		makeSut();
 		dbLoadAllEstablishments.loadAll();
 		Assert.assertTrue(loadAllEstablishmentsRepositorySpy.getWasCalled());
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void should_throw_if_LoadAllEstablishmentsRepository_throws() {
+		loadAllEstablishmentsRepositorySpy = Mockito.mock(LoadAllEstablishmentsRepositorySpy.class);
+		makeSut(loadAllEstablishmentsRepositorySpy);
+		when(loadAllEstablishmentsRepositorySpy.loadAll()).thenThrow(new RuntimeException());
+		dbLoadAllEstablishments.loadAll();
 	}
 
 }
