f699dedc20c4e7bcf0af02335274358f7acc49a9
Wisinewski <g_gomees@hotmail.com>
2021-07-15 15:34:59 -0300

    feat: create add vehicle usecase

---
+++ src/main/java/com/wisinewski/backendtestjava/domain/usecases/AddVehicle.java
@@ -0,0 +1,9 @@
+package com.wisinewski.backendtestjava.domain.usecases;
+
+import com.wisinewski.backendtestjava.domain.models.vehicle.Vehicle;
+
+public interface AddVehicle {
+	
+	void addVehicle(Vehicle vehicle);
+
+}
