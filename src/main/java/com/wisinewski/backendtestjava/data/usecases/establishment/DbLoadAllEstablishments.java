7f4501444ea5e3cfe836cedb14ec29fc4412300a
Wisinewski <g_gomees@hotmail.com>
2021-07-15 14:11:37 -0300

    feat: create LoadAllEstablishment controller

--- src/main/java/com/wisinewski/backendtestjava/data/usecases/establishment/DbLoadAllEstablishments.java
+++ src/main/java/com/wisinewski/backendtestjava/data/usecases/establishment/DbLoadAllEstablishments.java
@@ -3,11 +3,13 @@ package com.wisinewski.backendtestjava.data.usecases.establishment;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.wisinewski.backendtestjava.data.protocols.establishment.LoadAllEstablishmentsRepository;
 import com.wisinewski.backendtestjava.domain.models.establishment.Establishment;
 import com.wisinewski.backendtestjava.domain.usecases.LoadAllEstablishments;
 
+@Service
 public class DbLoadAllEstablishments implements LoadAllEstablishments {
 
 	private LoadAllEstablishmentsRepository loadAllEstablishmentsRepository;
