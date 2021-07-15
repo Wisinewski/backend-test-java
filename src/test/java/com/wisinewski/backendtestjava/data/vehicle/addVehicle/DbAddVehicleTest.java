23b4f102a864f2420b4ebd65ee6e79ef8d669288
Wisinewski <g_gomees@hotmail.com>
2021-07-15 15:52:03 -0300

    test: ensure DbAddVehicle throws if AddVehicleRepository throws

--- src/test/java/com/wisinewski/backendtestjava/data/vehicle/addVehicle/DbAddVehicleTest.java
+++ src/test/java/com/wisinewski/backendtestjava/data/vehicle/addVehicle/DbAddVehicleTest.java
@@ -1,7 +1,10 @@
 package com.wisinewski.backendtestjava.data.vehicle.addVehicle;
 
+import static org.mockito.Mockito.doThrow;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import com.wisinewski.backendtestjava.data.usecases.vehicle.DbAddVehicle;
 import com.wisinewski.backendtestjava.domain.models.VehicleTest;
@@ -18,12 +21,26 @@ public class DbAddVehicleTest {
 		dbAddVehicle = new DbAddVehicle(addVehicleRepositorySpy);
 	}
 	
+	public void makeSut(AddVehicleRepositorySpy addVehicleRepositorySpy) {
+		this.addVehicleRepositorySpy = addVehicleRepositorySpy;
+		dbAddVehicle = new DbAddVehicle(this.addVehicleRepositorySpy);
+	}
+	
 	@Test
 	public void should_call_AddVehicleRepository_with_correct_value() {
 		makeSut();
 		Vehicle vehicle = VehicleTest.mockVehicle();
 		dbAddVehicle.addVehicle(vehicle);
 		Assert.assertEquals(addVehicleRepositorySpy.getVehicle(), vehicle);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void should_throw_if_AddVehicleRepository_throws() {
+		addVehicleRepositorySpy = Mockito.mock(AddVehicleRepositorySpy.class);
+		makeSut(addVehicleRepositorySpy);
+		Vehicle vehicle = VehicleTest.mockVehicle();
+		doThrow(new RuntimeException()).when(addVehicleRepositorySpy).add(vehicle);
+		dbAddVehicle.addVehicle(vehicle);
 	}
 	
 }
