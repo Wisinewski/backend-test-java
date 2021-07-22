b37a95f6e591fc3fe1f3156de843b070f94ced1e
Wisinewski <g_gomees@hotmail.com>
2021-07-22 11:10:34 -0300

    refactor: ensure VehicleMySQL extends LoadAllVehiclesByEstablishmentRepository

--- src/main/java/com/wisinewski/backendtestjava/infra/repository/db/mysql/VehicleMySQLRepository.java
+++ src/main/java/com/wisinewski/backendtestjava/infra/repository/db/mysql/VehicleMySQLRepository.java
@@ -1,15 +1,25 @@
 package com.wisinewski.backendtestjava.infra.repository.db.mysql;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.wisinewski.backendtestjava.data.protocols.vehicle.AddVehicleRepository;
+import com.wisinewski.backendtestjava.data.protocols.vehicle.LoadAllVehiclesByEstablishmentRepository;
+import com.wisinewski.backendtestjava.domain.models.establishment.Establishment;
 import com.wisinewski.backendtestjava.domain.models.vehicle.Vehicle;
 
-public interface VehicleMySQLRepository extends JpaRepository<Vehicle, Long>, AddVehicleRepository {
+public interface VehicleMySQLRepository extends JpaRepository<Vehicle, Long>, AddVehicleRepository, LoadAllVehiclesByEstablishmentRepository {
 
 	@Override
 	public default void add(Vehicle vehicle) {
 		save(vehicle);
 	}
+	
+	@Override
+	@Query("SELECT v FROM Vehicle v WHERE v.establishment = :establishment")
+	public List<Vehicle> loadAllByEstablishment(@Param(value = "establishment") Establishment establishment);
 	
 }
